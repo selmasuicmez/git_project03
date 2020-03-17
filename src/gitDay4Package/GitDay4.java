be84210f806eb0c4576cdc8e06f2cae20e2f03a2
selmasuicmez <62161867+selmasuicmez@users.noreply.github.com>
2020-03-17 01:34:51 +0100

    changes from bahadir

--- src/gitDay4Package/GitDay4.java
+++ src/gitDay4Package/GitDay4.java
@@ -5,6 +5,7 @@ public class GitDay4 {
 	public static void main(String[] args) {
 	System.out.println("Local first commit master branch");
 System.out.println("local branch first commit");
+System.out.println("some one else contirbuted to the cod");		
 	}
 
 }
